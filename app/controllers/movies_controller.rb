605205cc2cd7e27cea3bf87c8ff6a8dacf15573f
“Alexandre <alexandreguignot@gmail.com>
2015-01-18 23:32:32 +0000

    json

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,11 +1,12 @@
 class MoviesController < ApplicationController
   before_action :set_movie, only: [:show, :edit, :update, :destroy]
+  require 'json'
 
   # GET /movies
   # GET /movies.json
   def index
     @movies = Movie.all
-    @res= Json.parse('http://api.tanktop.tv/api/1/eyJzZXJ2aWNlX2lkIjoxN30:1XKPst:glE97qRYa7ziwUHZN94AIjt5a3U/movie/35532')
+    @res= JSON.parse('http://api.tanktop.tv/api/1/eyJzZXJ2aWNlX2lkIjoxN30:1XKPst:glE97qRYa7ziwUHZN94AIjt5a3U/movie/35532')
   end
 
   # GET /movies/1
