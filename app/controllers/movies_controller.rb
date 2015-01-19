d45216fee792e54b28f5d9712089656e5801d686
“Alexandre <alexandreguignot@gmail.com>
2015-01-19 22:12:48 +0000

    json

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,6 +1,6 @@
 class MoviesController < ApplicationController
   before_action :set_movie, only: [:show, :edit, :update, :destroy]
-  requir 'httparty'
+  require 'httparty'
 
   # GET /movies
   # GET /movies.json
