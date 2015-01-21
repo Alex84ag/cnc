4a03744c7b846768e38b28c6d444430ee42c5c10
“Alexandre <alexandreguignot@gmail.com>
2015-01-21 00:57:45 +0000

    json

--- app/controllers/movie1s_controller.rb
+++ app/controllers/movie1s_controller.rb
@@ -1,6 +1,6 @@
 class Movie1sController < ApplicationController
   before_action :set_movie1, only: [:show, :edit, :update, :destroy]
-
+require 'open-uri'
   # GET /movie1s
   # GET /movie1s.json
   def index
