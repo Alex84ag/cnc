7f8939f6425c9a7ef0f85dd551b107fa6503cf6b
“Alexandre <alexandreguignot@gmail.com>
2015-02-15 23:09:57 +0000

    Movie1 update

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,8 @@ Rails.application.routes.draw do
   resources :movie_infos
 
   resources :movies
+  
+  root 'pages#home'
 
   get 'welcome/index'
 
@@ -16,7 +18,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'welcome#index' 
+  #root 'welcome#index' 
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
